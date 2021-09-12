a4706f9953d42a51f6cf8ca2350d0985a838174f
shuntagami <shun.tagami@i-nations.tokyo>
2021-09-12 18:24:27 +0900

    Impl Mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -5,8 +5,45 @@
 
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-//
-// This program only needs to handle arguments that satisfy
-// R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// 合計値を格納する sum を初期化
+@sum // sumはメモリの特定の場所を参照している
+M=0 // sum=0
+// ループ用のカウンタ i を初期化
+@i // iはメモリの特定の場所を参照している
+M=0 // i=0
+
+(LOOP)
+	// ループが目的の回数に達したらENDにジャンプする
+	@i
+	D=M // D=i
+	@R1
+	D=D-M // D=D-RAM[1]
+	@END
+	D;JEQ // If D=0 goto end
+
+	// 加算
+	@R0
+	D=M // D=RAM[0]
+	@sum
+	M=D+M // sum=sum+RAM[0]
+
+	// カウンタをインクリメント
+	@i
+	M=M+1
+	// ループの先頭に戻る
+	@LOOP
+	0;JMP
+
+@END
+D;JEQ
+(END)
+	// 結果sumをRAM(R2)に保存する
+	@sum
+	D=M // D=sum
+	@R2
+	M=D // RAM[2]=sum
+
+	// 終了
+	@END
+	0;JMP
